6878bbcd9eadf160ba89dd01ef0626f875b3adc6
JoichiSaito <joichisaito1994@gmail.com>
2021-02-28 08:31:09 +0900

    Rubocopによるコード修正

--- spec/controllers/boards_controller_spec.rb
+++ spec/controllers/boards_controller_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe BoardsController, type: :controller do
   let(:user_parent) { FactoryBot.create(:user_parent) }
   let(:board_1) { FactoryBot.create(:board_1, user_id: user_school.id) }
 
-  describe '#new' do    
+  describe '#new' do
     context 'ログインユーザー(学校)として' do
       it '正常にレスポンスを返すこと' do
         sign_in user_school
@@ -35,54 +35,54 @@ RSpec.describe BoardsController, type: :controller do
     context 'ログインユーザー(学校)として' do
       it 'お知らせを作成できること' do
         sign_in user_school
-        expect{
-          post :create, params: { 
+        expect  do
+          post :create, params: {
             board: {
               user_id: 1,
               title: '除草作業のお知らせ',
               content: '除草作業のお知らせ',
               quetionnaire: true
-            } 
+            }
           }
-        }.to change(Board, :count).by(1)
+        end.to change(Board, :count).by(1)
       end
 
       it 'お知らせ作成後に作成したお知らせの詳細ページへリダイレクトされること' do
         sign_in user_school
-        post :create, params: { 
+        post :create, params: {
           board: {
             user_id: 1,
             title: '除草作業のお知らせ',
             content: '除草作業のお知らせ',
             quetionnaire: true
-          } 
+          }
         }
         expect(response).to redirect_to(Board.last)
       end
 
-      it "不正なお知らせは作成できないこと" do
+      it '不正なお知らせは作成できないこと' do
         sign_in user_school
-        expect {
+        expect do
           post :create, params: {
             board: {
               user_id: 1,
               title: nil,
-              content: "除草作業のお知らせ",
+              content: '除草作業のお知らせ',
               quetionnaire: true
             }
           }
-        }.to_not change(Board, :count)
+        end.to_not change(Board, :count)
       end
 
       it '不正なお知らせを作成すると、再度作成ページへリダイレクトされること' do
         sign_in user_school
-        post :create, params: { 
+        post :create, params: {
           board: {
             user_id: 1,
             title: nil,
             content: '除草作業のお知らせ',
             quetionnaire: true
-          } 
+          }
         }
         expect(response).to redirect_to '/boards/new'
       end
@@ -95,7 +95,7 @@ RSpec.describe BoardsController, type: :controller do
         expect(response).to redirect_to root_path
       end
     end
-  
+
     context 'ゲストとして' do
       it 'サインイン画面にリダイレクトすること' do
         get :create
@@ -104,7 +104,7 @@ RSpec.describe BoardsController, type: :controller do
     end
   end
 
-  describe '#edit' do    
+  describe '#edit' do
     context 'ログインユーザー(学校)として' do
       it '正常にレスポンスを返すこと' do
         sign_in user_school
@@ -135,40 +135,40 @@ RSpec.describe BoardsController, type: :controller do
     end
   end
 
-  describe "#update" do
-    context "ログインユーザー(学校)として" do
-      it "正常に記事を更新できること" do
+  describe '#update' do
+    context 'ログインユーザー(学校)として' do
+      it '正常に記事を更新できること' do
         sign_in user_school
-        board_params = {title: "テスト"}
-        patch :update, params: {id: board_1.id, board: board_params}
-        expect(board_1.reload.title).to eq "テスト"
+        board_params = { title: 'テスト' }
+        patch :update, params: { id: board_1.id, board: board_params }
+        expect(board_1.reload.title).to eq 'テスト'
       end
 
-      it "記事を更新した後、更新された記事の詳細ページへリダイレクトすること" do
+      it '記事を更新した後、更新された記事の詳細ページへリダイレクトすること' do
         sign_in user_school
-        board_params = {title: "テスト"}
-        patch :update, params: {id: board_1.id, board: board_params}
+        board_params = { title: 'テスト' }
+        patch :update, params: { id: board_1.id, board: board_params }
         expect(response).to redirect_to(Board.last)
       end
 
-      it "不正なアトリビュートを含むお知らせは更新できないこと" do
+      it '不正なアトリビュートを含むお知らせは更新できないこと' do
         sign_in user_school
-        board_params = {title: nil}
-        patch :update, params: {id: board_1.id, board: board_params}
-        expect(board_1.reload.title).to eq "懇談会のお知らせ"
+        board_params = { title: nil }
+        patch :update, params: { id: board_1.id, board: board_params }
+        expect(board_1.reload.title).to eq '懇談会のお知らせ'
       end
 
-      it "不正なお知らせを更新しようとすると、再度更新ページへリダイレクトされること" do
+      it '不正なお知らせを更新しようとすると、再度更新ページへリダイレクトされること' do
         sign_in user_school
-        board_params = {title: nil}
-        patch :update, params: {id: board_1.id, board: board_params}
+        board_params = { title: nil }
+        patch :update, params: { id: board_1.id, board: board_params }
         expect(response).to redirect_to(Board.last)
       end
 
       it '他の学校がお知らせを更新しようとしたらトップページにリダイレクトすること' do
         sign_in user_school2
-        board_params = {title: "テスト"}
-        patch :update, params: {id: board_1.id, board: board_params}
+        board_params = { title: 'テスト' }
+        patch :update, params: { id: board_1.id, board: board_params }
         expect(response).to redirect_to root_path
       end
     end
@@ -178,81 +178,81 @@ RSpec.describe BoardsController, type: :controller do
         sign_in user_parent
         board_params = {
           user_id: 1,
-          title: "テスト",
-          content: "テスト",
+          title: 'テスト',
+          content: 'テスト',
           questionnaire: true
         }
-        patch :update, params: {id: board_1.id, board: board_params}
+        patch :update, params: { id: board_1.id, board: board_params }
         expect(response).to redirect_to root_path
       end
     end
-  
+
     context 'ゲストとして' do
       it 'サインイン画面にリダイレクトすること' do
         board_params = {
           user_id: 1,
-          title: "テスト",
-          content: "テスト",
+          title: 'テスト',
+          content: 'テスト',
           questionnaire: true
         }
-        patch :update, params: {id: board_1.id, board: board_params}
+        patch :update, params: { id: board_1.id, board: board_params }
         expect(response).to redirect_to '/users/sign_in'
       end
     end
   end
 
-  describe "#destroy" do
-    context "ログインユーザー(学校)として" do
-      it "正常に記事を削除できること" do
+  describe '#destroy' do
+    context 'ログインユーザー(学校)として' do
+      it '正常に記事を削除できること' do
         sign_in user_school
-        expect {
-          delete :destroy, params: {id: board_1.id}
-        }.to change(Board, :count).by(0)
+        expect do
+          delete :destroy, params: { id: board_1.id }
+        end.to change(Board, :count).by(0)
       end
 
-      it "記事を削除した後、マイページへリダイレクトされること" do
+      it '記事を削除した後、マイページへリダイレクトされること' do
         sign_in user_school
-        delete :destroy, params: {id: board_1.id}
+        delete :destroy, params: { id: board_1.id }
         expect(response).to redirect_to user_path(user_school)
       end
 
-      it "お知らせを作成したユーザー以外は、記事を削除できないこと" do
+      it 'お知らせを作成したユーザー以外は、記事を削除できないこと' do
         sign_in user_school2
         @another_board = user_school.boards.create(
           user_id: 1,
-          title: "テスト",
-          content: "テスト",
+          title: 'テスト',
+          content: 'テスト',
           questionnaire: true
         )
-        expect {
-          delete :destroy, params: {id: @another_board.id}
-        }.to_not change(user_school.boards, :count)
+        expect do
+          delete :destroy, params: { id: @another_board.id }
+        end.to_not change(user_school.boards, :count)
       end
 
-      it "お知らせを作成したユーザー以外が記事を削除しようとするとトップページにリダイレクトすること" do
+      it 'お知らせを作成したユーザー以外が記事を削除しようとするとトップページにリダイレクトすること' do
         sign_in user_school2
         @another_board = user_school.boards.create(
           user_id: 1,
-          title: "テスト",
-          content: "テスト",
+          title: 'テスト',
+          content: 'テスト',
           questionnaire: true
         )
-        delete :destroy, params: {id: @another_board.id}
+        delete :destroy, params: { id: @another_board.id }
         expect(response).to redirect_to root_path
       end
     end
 
-    context "ログインユーザー(保護者)として" do
-      it "トップページにリダイレクトされること" do
+    context 'ログインユーザー(保護者)として' do
+      it 'トップページにリダイレクトされること' do
         sign_in user_parent
-        delete :destroy, params: {id: board_1.id}
+        delete :destroy, params: { id: board_1.id }
         expect(response).to redirect_to root_path
       end
     end
 
     context 'ゲストとして' do
       it 'サインイン画面にリダイレクトすること' do
-        delete :destroy, params: {id: board_1.id}
+        delete :destroy, params: { id: board_1.id }
         expect(response).to redirect_to '/users/sign_in'
       end
     end
